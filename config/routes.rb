b5c855cd47d00115f940135b2a708a4df089743e
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-08-15 15:32:33 +0200

    As a user, I can add a new task

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   #read/list all tasks
   get '/tasks', to: 'tasks#index'
   #read the details of a task
+  get '/tasks/new', to: 'tasks#new', as: :new
   get '/tasks/:id', to: 'tasks#show', as: :task
   # #create a new task(2 steps: as a user see the form and after that create task)
-  # get '/tasks/new', to: 'tasks#new'
-  # post '/tasks', to: 'tasks#create'
+  
+  post '/tasks', to: 'tasks#create'
   # #as a user I can edit a task (mark as completed / update title & details)
   # #(display a form and after that update)
   # get '/tasks/:id/edit', to: 'tasks#edit'
