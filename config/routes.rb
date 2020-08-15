1bf523fdc2fcee13afae5f961b479c93987a1433
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-08-15 11:29:32 +0200

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  #read/list all tasks
+  get '/tasks', to: 'tasks#index'
+  #read the details of a task
+  get '/tasks:id', to: 'tasks#show'
+  #create a new task(2 steps: as a user see the form and after that create task)
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  #as a user I can edit a task (mark as completed / update title & details)
+  #(display a form and after that update)
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  #as a user delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+
+
+
+  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
